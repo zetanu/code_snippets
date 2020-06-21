615f2b09e2210cdf5c1f8a837a79d39eefb99a3f
zetanu <evan_ent@163.com>
2020-06-21 17:45:04 +0800

    feat(golang): dup

---
+++ golang/base/gopl/ch1/dup/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			// data, err := ioutil.ReadFile(f)  一次读取全部
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "err:%v\n", err)
+				continue
+			}
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+}
