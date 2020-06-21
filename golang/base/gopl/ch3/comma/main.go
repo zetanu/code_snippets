ed73054aa97749b2d211d21e88c10a45fc82fef9
zetanu <evan_ent@163.com>
2020-06-21 23:37:35 +0800

    feat(golang): comma

--- golang/base/gopl/ch3/comma/main.go
+++ golang/base/gopl/ch3/comma/main.go
@@ -1,9 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+)
 
 func main() {
 	fmt.Println(comma("123456"))
+	fmt.Println(comma2("1234567"))
 }
 
 func comma(s string) string {
@@ -12,4 +16,21 @@ func comma(s string) string {
 		return s
 	}
 	return comma(s[:n-3]) + "," + s[n-3:]
+}
+
+func comma2(s string) string {
+	var buf bytes.Buffer
+	start := len(s) % 3
+
+	if start > 0 {
+		buf.WriteString(s[:start])
+		buf.WriteByte(',')
+	}
+	for ; start < len(s); start += 3 {
+		buf.WriteString(s[start : start+3])
+		if start != len(s)-3 {
+			buf.WriteByte(',')
+		}
+	}
+	return buf.String()
 }
