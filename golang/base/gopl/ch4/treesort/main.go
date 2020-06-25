52bb0628a299ac509c9e09f52f69b1d5224b8415
zetanu <evan_ent@163.com>
2020-06-25 09:26:54 +0800

    feat(golang): treesort

---
+++ golang/base/gopl/ch4/treesort/main.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+type tree struct {
+	value       int
+	left, right *tree
+}
+
+func Sorts(values []int) {
+	var root *tree
+	for _, v := range values {
+		root = add(root, v)
+	}
+	appendValues(values[:0], root)
+}
+
+func appendValues(values []int, t *tree) []int {
+	if t != nil {
+		values = appendValues(values, t.left)
+		values = append(values, t.value)
+		values = appendValues(values, t.right)
+	}
+	return values
+}
+
+func add(t *tree, value int) *tree {
+	if t == nil {
+		t = new(tree)
+		t.value = value
+		return t
+	}
+
+	if value < t.value {
+		t.left = add(t.left, value)
+	} else {
+		t.right = add(t.right, value)
+	}
+	return t
+}
+
+func main() {
+	a := []int{9, 4, 5}
+	Sorts(a)
+	fmt.Printf("%v\n", a)
+}
